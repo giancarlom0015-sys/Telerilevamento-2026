08eb6636d8419283c510a288184dae0ee31050aa
giancarlom0015-sys <giancarlom0015@gmail.com>
2026-04-16 14:40:57 +0200

    Create externaldata_exam_example_06.R

---
+++ code/externaldata_exam_example_06.R
@@ -0,0 +1,9 @@
+#questo serve per scaricare dei dati
+#https://code.earthengine.google.com/957b34097e4d6f4dc33ace082ec7cad7 clicchi questo
+#apri la pagina fai nuovo progetto e inserisci pezzo di codice troviamo un codice "var aoi" lo eliminiamo cestinandolo
+#poi creiamo un area of interest, si crea un poligono rettangolare con un tool
+#si scrive al posto che geometry, "aoi" poi prenderà la mediana cambiando le da
+#si clicca run e trova 
+#si va nell' area a destra e si fanno partire i dati con run, che sono pesanti e vengono salvati nel drive
+
+#questo ci trova tutti i dati sentinel nello spazio ed estre la mediana
