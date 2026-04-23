45f42f4503328cefb9d800fa3198bd2af5ac840a
giancarlom0015-sys <giancarlom0015@gmail.com>
2026-04-23 15:02:17 +0200

    Create 08_functions.R

---
+++ code/08_functions.R
@@ -0,0 +1,17 @@
+#le mie funzioni!! posso scrivermele yeee
+
+
+somma <- function (x,y)  {
+  z=x+y
+  return(z)
+ 
+}
+ #esercizio make the funcio called difference
+
+differenzapazza <- function (x,y)  {
+  z=x-y
+  return(z)
+ 
+}
+
+im.classify 
